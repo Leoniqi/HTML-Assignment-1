90b758cac75c1247729d709138d140e57ebd4435
Leoniqi <leo.vuniqi55@gmail.com>
2018-02-09 18:18:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function EnterEvenNums(){
+    var evenNumAmount = prompt("How many even numbers do you want to enter?");
+    for(var i=0; i<evenNumAmount;i++){
+        var evenNums = prompt("");
+    }
+}
